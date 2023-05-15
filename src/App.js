c2bc9d5247b0cac0dfc880c07354eee586e6a434
dheena <deenatamilnathan007@gmail.com>
2023-05-15 10:29:29 +0530

    add front page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUp from './components/signup/signup';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 import Navbar from './pages/navbar';
+import Home from './pages/home';
 function App() {
 
   // const currentUser = true;
@@ -22,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        
+          <Route path='/' element={<Home />}></Route>
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/navbar' element={<Navbar />}></Route>
---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import Build from "../assests/build.png"
+
+
+const Home = ()=>{
+    return(
+        <div className="flex flex-row">
+       <div>
+        <img src={Build} alt="cool" className="w-[18rem] h-[18rem] mx-[12rem] my-[12rem] animate-pulse rounded" />
+       </div>
+       <Link to={"/login"}><div className="mx-[9rem] my-[18rem] text-3xl text-cyan-300 hover:text-cyan-900  italic  cursor-pointer hover:font-bold">
+        LET'S START
+      </div></Link>
+       </div>
+
+    )
+}
+
+
+export default Home;
